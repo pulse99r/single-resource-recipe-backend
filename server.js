8edb1d494e8a18dd564ccc9c59a7ed4c33cd577f
Raymond <raymondjohnson@pursuit.org>
2023-08-15 19:38:42 -0400

    Create Recipe route still not working

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT;
 
 // LISTEN
 app.listen(PORT, () => {
-  console.log(`Tuner Server is Listening on port ${PORT}`);
+  console.log(`Recipe Backend Server is Listening on port ${PORT}`);
 });
